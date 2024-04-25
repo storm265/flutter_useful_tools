61020fb5530108b38c286641f6517a78ff0fb998
storm265 <peter4533@mail.ru>
2024-04-25 23:30:29 +0300

    refactor->extension

--- lib/tools/is_light_theme.dart
+++ lib/tools/is_light_theme.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-bool isLightTheme({required BuildContext context}) => Platform.isIOS
-    ? CupertinoTheme.of(context).brightness == Brightness.light
-    : Theme.of(context).brightness == Brightness.light;
+extension IsLightTheme on BuildContext {
+  bool isLightTheme() => Platform.isIOS
+      ? CupertinoTheme.of(this).brightness == Brightness.light
+      : Theme.of(this).brightness == Brightness.light;
+}
